c12d4e0c76431cead3a909c852e3f40e5ef4ccff
Anand0810 <anandkumar0810@gmail.com>
2017-01-28 16:48:33 +0530

    Java Object to json added

--- Demo/src/main/java/com/projectX/controller/HelloController.java
+++ Demo/src/main/java/com/projectX/controller/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projectX.beans.ResponseJsonBean;
 
 //import com.projectX.beans.Quote;
@@ -26,8 +28,12 @@ public class HelloController {
 		String uri = "http://gturnquist-quoters.cfapps.io/api/random";
 	    RestTemplate restTemplate = new RestTemplate();
 	    ResponseJsonBean result = restTemplate.getForObject(uri, ResponseJsonBean.class);
-	    //Quote result = restTemplate.getForObject(uri, Quote.class);
-	    
-	    return new ModelAndView("userpage","result", result.getValue());
+	    ObjectMapper mapper = new ObjectMapper();
+	    try {
+			return new ModelAndView("userpage","result", mapper.writeValueAsString(result));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return new ModelAndView("userpage","result", e.getMessage());
+		}
 	}
 }
